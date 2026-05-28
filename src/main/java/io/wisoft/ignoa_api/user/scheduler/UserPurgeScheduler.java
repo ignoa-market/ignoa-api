9c5fe2c065bc8035b2d5b99916a4cc6e8597b8b9
Woomin-Wang <wwmin828@gmail.com>
2026-05-28 13:13:07 +0900

    feat: 스케줄러 중복 실행 방지 ShedLock 적용

--- src/main/java/io/wisoft/ignoa_api/user/scheduler/UserPurgeScheduler.java
+++ src/main/java/io/wisoft/ignoa_api/user/scheduler/UserPurgeScheduler.java
@@ -3,6 +3,7 @@ package io.wisoft.ignoa_api.user.scheduler;
 import io.wisoft.ignoa_api.user.service.UserPurgeJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ public class UserPurgeScheduler {
     private final UserPurgeJob userPurgeJob;
 
     @Scheduled(cron = "0 0 1 * * *")
+    @SchedulerLock(name = "userPurgeScheduler")
     public void purgeExpiredWithdrawals() {
         log.info("탈퇴 회원 개인정보 파기 스케줄러 실행");
         userPurgeJob.execute();
