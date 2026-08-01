1ddd89c6a953c419d8c4013a69b997adf3972b12
Woomin-Wang <wwmin828@gmail.com>
2026-08-01 22:56:29 +0900

    test(load): 입찰 waitTime 측정을 위해 대기시간 임시 조정

--- src/main/java/io/wisoft/ignoa_api/bid/service/BidFacade.java
+++ src/main/java/io/wisoft/ignoa_api/bid/service/BidFacade.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BidFacade {
 
-    private static final long BID_WAIT_MILLIS = 250L;
+    private static final long BID_WAIT_MILLIS = 1000L;
 
     private final RedissonDistributedLock distributedLock;
     private final BidService bidService;
