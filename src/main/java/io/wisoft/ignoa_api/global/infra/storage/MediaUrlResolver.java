7c95978555547f77f8cc30053c4826ce82f7cbee
Woomin-Wang <wwmin828@gmail.com>
2026-09-01 01:28:57 +0900

    feat(storage): 미디어 조회 URL을 CloudFront로 전환

--- src/main/java/io/wisoft/ignoa_api/global/infra/storage/MediaUrlResolver.java
+++ src/main/java/io/wisoft/ignoa_api/global/infra/storage/MediaUrlResolver.java
@@ -1,19 +1,14 @@
 package io.wisoft.ignoa_api.global.infra.storage;
 
 import io.wisoft.ignoa_api.user.entity.ProfileImageSource;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @Component
-@RequiredArgsConstructor
 public class MediaUrlResolver {
 
-    private final S3Client s3Client;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    @Value("${cloud.aws.cloudfront.media-base-url}")
+    private String mediaBaseUrl;
 
     // 외부 URL(카카오)과 S3 Object Key가 함께 저장될 수 있는 프로필 이미지에 사용
     public String toUrl(String reference, ProfileImageSource source) {
@@ -25,11 +20,7 @@ public class MediaUrlResolver {
             return reference;
         }
 
-        return s3Client.utilities()
-                .getUrl(builder -> builder
-                        .bucket(bucket)
-                        .key(reference))
-                .toExternalForm();
+        return "%s/%s".formatted(mediaBaseUrl, reference);
     }
 
     // 항상 S3 Object Key를 저장하는 상품 이미지에 사용
