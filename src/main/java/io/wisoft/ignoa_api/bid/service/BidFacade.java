6d0ea38710bebf94f7a60b738fe5dc906c0c9dd8
Woomin Wang <wwmin828@gmail.com>
2026-09-15 23:19:07 +0900

    test: waitTime 200ms 비교 측정을 위한 임시 변경 (#161)

--- src/main/java/io/wisoft/ignoa_api/bid/service/BidFacade.java
+++ src/main/java/io/wisoft/ignoa_api/bid/service/BidFacade.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BidFacade {
 
-    private static final long BID_WAIT_MILLIS = 250L;
+    private static final long BID_WAIT_MILLIS = 200L;
 
     private final RedissonDistributedLock distributedLock;
     private final BidService bidService;
